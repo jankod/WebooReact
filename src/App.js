2bca910bb8c8466805c7142f7f888bc765b5e843
Janko Diminic <janko.diminic@sedamit.hr>
2023-02-23 18:15:43 +0100

    save all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {createElement} from "react";
+import { Route, Switch } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Weboo {
+
+
+
+}
+const weboo = new Weboo();
+weboo.addPage()
+
+const components = {
+    MyButton: MyButton
 }
 
-export default App;
+const page1 = {}
+
+function MyButton() {
+    return (
+        <button>Ovo je button</button>
+    );
+}
+
+const renderComponent = config => {
+    if (typeof componentNames[config.component] !== 'undefined') {
+        return createElement(componentNames[config.component], config, config.children && (typeof config.children === 'string' ? config.children : config.children.map(c => renderComponent(c))),)
+    }
+}
+
+
+function Page1() {
+
+    return createElement(MyButton, {id: 'nbeki223'}, 'Ovo je pocetak')
+}
+
+
+export default Page1;
